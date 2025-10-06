f67aa9249b960a266d1631d6c5948b1fd52af53e
hassam571 <168352683+hassam571@users.noreply.github.com>
2025-10-06 08:39:09 +0500

    Hassam Changes

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -40,7 +40,7 @@ class OAuthController extends Controller
             $error = $request->query('error');
             $desc = $request->query('error_description', 'Authorization failed');
             
-            \Log::error("OAuth Error - {$provider}", [
+            Log::error("OAuth Error - {$provider}", [
                 'error' => $error,
                 'description' => $desc,
                 'query' => $request->query()
@@ -164,7 +164,7 @@ class OAuthController extends Controller
             return redirect()->route('tenant.profile', ['username' => $user->username]);
 
         } catch (\Exception $e) {
-            \Log::error("OAuth Callback Error - {$provider}", [
+            Log::error("OAuth Callback Error - {$provider}", [
                 'error' => $e->getMessage(),
                 'trace' => $e->getTraceAsString()
             ]);
